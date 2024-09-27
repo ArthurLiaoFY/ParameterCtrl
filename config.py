cdc6f174f04c04b87aa0b76343ea503f2ad116a5
arthur <wr80340@smail.nchu.edu.tw>
2024-09-27 11:13:37 +0800

    #add ddpg config

--- config.py
+++ config.py
@@ -21,6 +21,18 @@ q_learning_kwargs = {
     "fully_explore_step": 2000,
 }
 
+ddpg_kwargs = {
+    # -------------------------------
+    "learning_rate": 0.1,
+    "explore_rate": 0.5,
+    "learning_rate_min": 0.03,
+    "explore_rate_min": 0.03,
+    "learning_rate_decay": 0.999,
+    "explore_rate_decay": 0.999,
+    "discount_factor": 0.99,
+    "fully_explore_step": 2000,
+}
+
 # Env
 env_kwargs = {
     # ---------------
@@ -51,4 +63,5 @@ training_kwargs = {
     "step_per_episode": 201,
     "env_kwargs": env_kwargs,
     "q_learning_kwargs": q_learning_kwargs,
+    "ddpg_kwargs": ddpg_kwargs,
 }
