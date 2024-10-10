2e70fea2a77ad8aab8599ebef3cfcb6dd0002fdf
arthur <wr80340@smail.nchu.edu.tw>
2024-10-10 14:13:18 +0800

    #remove unused config

--- config.py
+++ config.py
@@ -27,11 +27,8 @@ ddpg_kwargs = {
     "action_dim": 2,
     # -------------------------------
     "learning_rate": 3e-4,
-    "explore_rate": 0.5,
-    "learning_rate_min": 0.03,
-    "explore_rate_min": 0.03,
+    "learning_rate_min": 3e-5,
     "learning_rate_decay": 0.999,
-    "explore_rate_decay": 0.999,
     "discount_factor": 0.99,
     "tau": 0.001,
     "buffer_size": 1e7,
