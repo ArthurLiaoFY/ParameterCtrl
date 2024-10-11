b0c303e6c47a3a98ee8083ad1b0a2a16e3e75f7a
arthur <wr80340@smail.nchu.edu.tw>
2024-10-11 17:20:52 +0800

    #modify config value

--- config.py
+++ config.py
@@ -31,9 +31,9 @@ ddpg_kwargs = {
     "learning_rate_min": 3e-4,
     "learning_rate_decay_factor": 0.999,
     "discount_factor": 0.99,
-    "jitter_noise": 0.999,
+    "jitter_noise": 0.5,
     "jitter_noise_min": 1e-5,
-    "jitter_noise_decay_factor": 0.99,
+    "jitter_noise_decay_factor": 1 - 1e-4,
     "tau": 0.001,
     # -------------------------------
     "buffer_size": 1e7,
