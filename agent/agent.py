add257395d6d7afd2f6b24c56ff7bb8d60e1c77e
arthur <wr80340@smail.nchu.edu.tw>
2025-03-05 17:38:09 +0800

    #add agent common class

---
+++ agent/agent.py
@@ -0,0 +1,42 @@
+from abc import ABC, abstractmethod
+
+import torch
+from tensordict import TensorDict
+
+
+class RLAgent(ABC):
+    def __init__(self, **kwargs) -> None:
+        self.start_explore
+        self.__dict__.update(**kwargs)
+
+    @abstractmethod
+    def select_action(self, state: torch.Tensor):
+        pass
+
+    @abstractmethod
+    def update_policy(self, sample_batch: TensorDict):
+        pass
+
+    def update_lr(self) -> None:
+        self.learning_rate = max(
+            self.learning_rate_min,
+            self.learning_rate * self.learning_rate_decay_factor,
+        )
+
+    @abstractmethod
+    def save_network(
+        self,
+        model_file_path: str,
+        prefix: str,
+        suffix: str,
+    ):
+        pass
+
+    @abstractmethod
+    def load_network(
+        self,
+        model_file_path: str,
+        prefix: str,
+        suffix: str,
+    ):
+        pass
