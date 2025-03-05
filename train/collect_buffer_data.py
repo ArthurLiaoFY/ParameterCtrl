84e08027be174256403942c53e27c16714f64af2
arthur <wr80340@smail.nchu.edu.tw>
2025-03-05 17:38:24 +0800

    #remove init

--- train/collect_buffer_data.py
+++ train/collect_buffer_data.py
@@ -23,58 +23,6 @@ class CollectBufferData:
         )
         self.load_replay_buffer()
 
-    def init_buffer_data(self, init_amount: int) -> None:
-        for _ in tqdm(range(init_amount)):
-            self.env.reset()
-            normed_state_list = []
-            action_list = []
-            reward_list = []
-            next_normed_state_list = []
-
-            for _ in range(self.step_per_episode):
-                normed_state_list.append(
-                    tuple(v for v in self.env.normed_state.values())
-                )
-                action = np.array(
-                    [
-                        np.random.uniform(
-                            low=self.env_kwargs.get("lower_F"),
-                            high=self.env_kwargs.get("upper_F"),
-                            size=1,
-                        ).item()
-                        - self.env.state.get("current_F"),
-                        np.random.uniform(
-                            low=self.env_kwargs.get("lower_Q"),
-                            high=self.env_kwargs.get("upper_Q"),
-                            size=1,
-                        ).item()
-                        - self.env.state.get("current_Q"),
-                    ]
-                )
-
-                action_list.append(self.env.norm_action(action))
-                reward_list.append(self.env.step(action=action))
-                next_normed_state_list.append(
-                    tuple(v for v in self.env.normed_state.values())
-                )
-
-            self.replay_buffer.extend(
-                TensorDict(
-                    {
-                        "state": torch.Tensor(np.array(normed_state_list)),
-                        "action": torch.Tensor(np.array(action_list)),
-                        "reward": torch.Tensor(np.array(reward_list)),
-                        "next_state": torch.Tensor(np.array(next_normed_state_list)),
-                        "priority": torch.Tensor(-1 * np.array(reward_list)),
-                    },
-                    batch_size=[self.step_per_episode],
-                )
-            )
-            self.replay_buffer.update_priority(
-                index=torch.tensor([i for i in range(self.step_per_episode)]),
-                priority=torch.Tensor(-1 * np.array(reward_list)),
-            )
-
     def extend_buffer_data(
         self, state: list, action: list, reward: list, next_state: list
     ) -> None:
@@ -86,14 +34,13 @@ class CollectBufferData:
                     "action": torch.Tensor(np.array(action)),
                     "reward": torch.Tensor(np.array(reward)),
                     "next_state": torch.Tensor(np.array(next_state)),
-                    "priority": torch.Tensor(-1 * np.array(reward)),
                 },
                 batch_size=[len(reward)],
             )
         )
         self.replay_buffer.update_priority(
             index=torch.tensor(
-                [i for i in range(self.replay_buffer.__len__(), len(reward))]
+                [self.replay_buffer.__len__() + i for i in range(len(reward))]
             ),
             priority=torch.Tensor(-1 * np.array(reward)),
         )
