67c24a3dda59a095d5b2d090ed38083a3eed6b06
arthur <wr80340@smail.nchu.edu.tw>
2025-03-05 15:22:47 +0800

    #set env out

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
 # %%
+from agent.ddpg_agent import DeepDeterministicPolicyGradient
 from config import training_kwargs
-from cstr_env import CSTREnv, np
-from train.collect_buffer_data import CollectBufferData
+from cstr_env import CSTREnv
 from train.train_ddpg import TrainAgent
 
-env = CSTREnv(seed=1122, **training_kwargs.get("env_kwargs"))
-buffer_data = CollectBufferData(env=env, **training_kwargs)
-buffer_data.init_buffer_data(init_amount=10)
+env = CSTREnv(**training_kwargs.get("env_kwargs"))
+agent = DeepDeterministicPolicyGradient(**training_kwargs.get("ddpg_kwargs"))
 # %%
-a = buffer_data.sample_buffer_data(size=20000)
-# %%
-tddpg = TrainAgent(**training_kwargs)
+
+tddpg = TrainAgent(env=env, agent=agent, **training_kwargs)
 tddpg.train_online_agent(
-    buffer_data=buffer_data,
     save_traj_to_buffer=False,
     save_network=True,
 )
+# %%
