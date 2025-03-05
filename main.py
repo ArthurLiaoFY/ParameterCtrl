770f627ccd969de4deb562cd433715d1d3cf4998
arthur <wr80340@smail.nchu.edu.tw>
2025-03-05 13:54:26 +0800

    #modify process

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
+# %%
 from config import training_kwargs
+from cstr_env import CSTREnv, np
 from train.collect_buffer_data import CollectBufferData
-from train.train_ddpg import TrainDDPG
+from train.train_ddpg import TrainAgent
 
-buffer_data = CollectBufferData(**training_kwargs)
-# buffer_data.extend_buffer_data(extend_amount=1000)
-
-tddpg = TrainDDPG(**training_kwargs)
+env = CSTREnv(seed=1122, **training_kwargs.get("env_kwargs"))
+buffer_data = CollectBufferData(env=env, **training_kwargs)
+buffer_data.init_buffer_data(init_amount=10)
+# %%
+a = buffer_data.sample_buffer_data(size=20000)
+# %%
+tddpg = TrainAgent(**training_kwargs)
 tddpg.train_online_agent(
     buffer_data=buffer_data,
     save_traj_to_buffer=False,
