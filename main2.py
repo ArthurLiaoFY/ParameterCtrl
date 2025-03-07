588cf35b51b75065dab8573def471630ba13afe4
arthur <wr80340@smail.nchu.edu.tw>
2025-03-07 08:19:30 +0800

    #add different env case

--- main2.py
+++ main2.py
@@ -2,17 +2,16 @@
 from agent.ddpg_agent import DeepDeterministicPolicyGradient
 from agent.dqn_agent import DeepQNetwork
 from config import training_kwargs
-from env.cstr_env import CSTREnv
-from train.train_cstr import TrainAgent
+from env.cartpole_env import CartPole
+from train.train_cartpole import TrainAgent
 
-env = CSTREnv(**training_kwargs.get("env_kwargs"))
-# agent = DeepDeterministicPolicyGradient(**training_kwargs.get("agent_kwargs"))
+env = CartPole()
+
 agent = DeepQNetwork(**training_kwargs.get("agent_kwargs"))
-# %%
-
 tddpg = TrainAgent(env=env, agent=agent, **training_kwargs)
 tddpg.train_online_agent(
     save_traj_to_buffer=False,
     save_network=False,
 )
+
 # %%
