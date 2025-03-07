848a1dfe2184723605a2e0d635f681f7c8b88393
arthur <wr80340@smail.nchu.edu.tw>
2025-03-07 09:06:38 +0800

    #main flow change

--- main.py
+++ main.py
@@ -5,9 +5,17 @@ from config import training_kwargs
 from env.cstr_env import CSTREnv
 from train.train_cstr import TrainAgent
 
-env = CSTREnv(**training_kwargs.get("env_kwargs"))
-# agent = DeepDeterministicPolicyGradient(**training_kwargs.get("agent_kwargs"))
-agent = DeepQNetwork(**training_kwargs.get("agent_kwargs"))
+env = CSTREnv(**training_kwargs.get("cstr_env_kwargs"))
+agent = DeepDeterministicPolicyGradient(
+    state_dim=training_kwargs.get("cstr_env_kwargs").get("state_dim"),
+    action_dim=training_kwargs.get("cstr_env_kwargs").get("action_dim"),
+    **training_kwargs.get("agent_kwargs")
+)
+# agent = DeepQNetwork(
+#     state_dim=training_kwargs.get("cstr_env_kwargs").get("state_dim"),
+#     action_dim=training_kwargs.get("cstr_env_kwargs").get("action_dim"),
+#     **training_kwargs.get("agent_kwargs")
+# )
 # %%
 
 tddpg = TrainAgent(env=env, agent=agent, **training_kwargs)
--- main2.py
+++ main2.py
@@ -1,5 +1,4 @@
 # %%
-from agent.ddpg_agent import DeepDeterministicPolicyGradient
 from agent.dqn_agent import DeepQNetwork
 from config import training_kwargs
 from env.cartpole_env import CartPole
@@ -7,9 +6,13 @@ from train.train_cartpole import TrainAgent
 
 env = CartPole()
 
-agent = DeepQNetwork(**training_kwargs.get("agent_kwargs"))
-tddpg = TrainAgent(env=env, agent=agent, **training_kwargs)
-tddpg.train_online_agent(
+agent = DeepQNetwork(
+    state_dim=training_kwargs.get("cartpole_env_kwargs").get("state_dim"),
+    action_dim=training_kwargs.get("cartpole_env_kwargs").get("action_dim"),
+    **training_kwargs.get("agent_kwargs")
+)
+tdqn = TrainAgent(env=env, agent=agent, **training_kwargs)
+tdqn.train_online_agent(
     save_traj_to_buffer=False,
     save_network=False,
 )
